0dd03b13127a303e37c059d7b603c5ff251a16c5
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-05-09 00:44:03 +0630

    'add-slider'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
+const slider = document.querySelector('#slider');
+const leftArrow = slider.querySelector('.left-arrow')
+const rightArrow = slider.querySelector('.right-arrow')
+const images = Array.from(slider.querySelectorAll('.slider-item'))
+let counter = 0;
+
+images.forEach((img, i) => {
+    img.style.left = `${i * 100}%`
+})
+
+function slide() {
+    images.forEach((img) => {
+        img.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
+
+leftArrow.onclick = function () {
+    if (counter === 0) return
+    counter += -1
+    slide()
+}
+
+rightArrow.onclick = function () {
+    if (counter === images.length - 1) return
+    counter += 1
+    slide()
+}
+
+
 const skeletonContianer = document.querySelectorAll('.skeleton-container');
-
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -20,3 +47,4 @@ const observer = new IntersectionObserver((entries) => {
 skeletonContianer.forEach((c) => {
     observer.observe(c);
 })
+
