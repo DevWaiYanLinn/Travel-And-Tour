08e03bd77e889d2485ccde340e36a7e5f5b5f13c
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-05-13 12:10:15 +0630

    fix

--- js/app.js
+++ js/app.js
@@ -4,6 +4,58 @@ const rightArrow = slider.querySelector('.right-arrow')
 const sliderItems = Array.from(slider.querySelectorAll('.slider-item'))
 const mobileMenu = document.querySelector('.mobile-menu')
 const navList = document.querySelector('.nav-list')
+// const jpMap = document.querySelector('.jp-map')
+// const paths = Array.from(jpMap.querySelectorAll('path'))
+
+const data = {
+    "JP01": "Hokkaidō", 
+    "JP02": "Aomori", 
+    "JP03": "Iwate", 
+    "JP04": "Miyagi", 
+    "JP05": "Akita", 
+    "JP06": "Yamagata", 
+    "JP07": "Fukushima", 
+    "JP08": "Ibaraki", 
+    "JP09": "Tochigi", 
+    "JP10": "Gunma", 
+    "JP11": "Saitama", 
+    "JP12": "Chiba", 
+    "JP13": "Tokyo", 
+    "JP14": "Kanagawa", 
+    "JP15": "Niigata", 
+    "JP16": "Toyama", 
+    "JP17": "Ishikawa", 
+    "JP18": "Fukui", 
+    "JP19": "Yamanashi", 
+    "JP20": "Nagano", 
+    "JP21": "Gifu", 
+    "JP22": "Shizuoka", 
+    "JP23": "Aichi", 
+    "JP24": "Mie", 
+    "JP25": "Shiga", 
+    "JP26": "Kyōto", 
+    "JP27": "Ōsaka", 
+    "JP28": "Hyōgo", 
+    "JP29": "Nara", 
+    "JP30": "Wakayama", 
+    "JP31": "Tottori", 
+    "JP32": "Shimane", 
+    "JP33": "Okayama", 
+    "JP34": "Hiroshima", 
+    "JP35": "Yamaguchi", 
+    "JP36": "Tokushima", 
+    "JP37": "Kagawa", 
+    "JP38": "Ehime", 
+    "JP39": "Kōchi", 
+    "JP40": "Fukuoka", 
+    "JP41": "Saga", 
+    "JP42": "Nagasaki", 
+    "JP43": "Kumamoto", 
+    "JP44": "Ōita", 
+    "JP45": "Miyazaki", 
+    "JP46": "Kagoshima", 
+    "JP47": "Okinawa"
+}
 
 mobileMenu.onclick = () => {
     navList.classList.toggle('active')
@@ -55,7 +107,13 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
+
 skeletonContianer.forEach((c) => {
     observer.observe(c);
 })
-
+// paths.forEach((p) => {
+//     p.onclick = function(e) {
+//         const id = e.target.id
+//         console.log(data[id])
+//     }
+// })
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 mix.js('js/app.js', 'js/script.js').css('css/app.css', 'css/style.css')
-
+.options({
+    processCssUrls: false
+});
 mix.webpackConfig({
     target: ['web', 'es5'],
     resolve: {
